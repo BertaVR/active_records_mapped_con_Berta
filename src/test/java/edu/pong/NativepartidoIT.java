0671abdea1f7772134098bad827e6744583e3fdd
JoanRibot <jribot@cifpfbmoll.eu>
2021-06-07 17:51:17 +0200

    comiteadno

---
+++ src/test/java/edu/pong/NativepartidoIT.java
@@ -0,0 +1,9 @@
+package edu.pong;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativepartidoIT extends partidoTest {
+
+    // Execute the same tests but in native mode.
+}
